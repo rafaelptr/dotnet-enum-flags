398df822ecd946ce4b1be20be6170949e90e744b
Rafael Petronilio <rafaelpetronilio@gmail.com>
2020-02-05 21:33:36 -0300

    Enum Flags Sample

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EnumFlagsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enum1 enum1 = Enum1.MyValue | Enum1.OtherValue;
+            Enum1Flags enum1Flags = Enum1Flags.MyValue | Enum1Flags.OtherValue;
+
+            //Enum1: 3
+            Console.WriteLine($"Enum1      : {enum1}");
+            //Enum1Flags: MyValue, OtherValue
+            Console.WriteLine($"Enum1Flags : {enum1Flags}");
+
+            Console.ReadKey();
+        }
+
+        enum Enum1 : long { MyValue = 1, OtherValue = 2 }
+
+        [Flags]
+        enum Enum1Flags : long { MyValue = 1, OtherValue = 2 }
+    }
+}
